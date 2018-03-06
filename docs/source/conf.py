f6aa74e15ff10b91c9ee056c7b62679ef9d1cca6
Luis Minero <luisminerog@gmail.com>
2018-03-05 22:36:51 -0600

    docs: adds Sphinx autosummary extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,8 @@ sys.path.insert(0, os.path.abspath('../../pyscholar'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx'
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autosummary'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
